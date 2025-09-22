a8cc215a3606feb65f23cbe06cb2cd0bf72698b2
Piioni <rangelc.juane@gmail.com>
2025-09-22 17:44:51 +0200

    New cliente exercises

--- app/src/view/pages/cliente/3/fecha.php
+++ app/src/view/pages/cliente/3/fecha.php
@@ -8,5 +8,14 @@ include $VIEW_DIR . '/layouts/__header.php';
 <p> Este ejercicio ha mostrado la fecha y hora actual. </p>
 
 <script>
-    
+    document.addEventListener("DOMContentLoaded", function() {
+        const fechaHoraActual = new Date();
+        const opciones = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+        const fechaHoraFormateada = fechaHoraActual.toLocaleDateString('es-ES', opciones);
+        document.body.insertAdjacentHTML('beforeend', `<p>Fecha y hora actual: ${fechaHoraFormateada}</p>`);
+    })
 </script>
+
+<?php
+include $VIEW_DIR . '/layouts/__footer.php';
+?>
--- app/src/view/pages/cliente/home_cliente.php
+++ app/src/view/pages/cliente/home_cliente.php
@@ -3,18 +3,26 @@ $title = 'Homepage Cliente';
 global $VIEW_DIR;
 include $VIEW_DIR . '/layouts/__header.php';
 ?>
-<h1>Homepage Cliente</h1>
-<p> Esta es la pagina principal de la asignatura de cliente. </p>
-<p> Desde aqui se podra navegar a todos los ejercicios de la asignatura. </p>
-<p> Los ejercicios de cliente son los siguientes: </p>
-<h2> Ejercicios #1 </h2>
-<ul>
-    <li><a href="/cliente/1/guess_number" >Ejercicio 1: Adivinar el numero</a></li>
-    <li><a href="/cliente/1/area_rectangulo" >Ejercicio 2: Area de un rectangulo</a></li>
-    <li><a href="/cliente/1/conversor_grados" >Ejercicio 3: Conversor de grados</a></li>
-</ul>
 
-<h2> Ejercicios #3 </h2>
-<ul>
-    <li><a href="/cliente/3/fecha"> Ejercicio 1: Fecha y hora actual </a></li>
-</ul>
+    <h1>Homepage Cliente</h1>
+    <p> Esta es la pagina principal de la asignatura de cliente. </p>
+    <p> Desde aqui se podra navegar a todos los ejercicios de la asignatura. </p>
+    <p> Los ejercicios de cliente son los siguientes: </p>
+    <h2> Ejercicios #1 </h2>
+    <ul>
+        <li><a href="/cliente/1/guess_number">Ejercicio 1: Adivinar el numero</a></li>
+        <li><a href="/cliente/1/area_rectangulo">Ejercicio 2: Area de un rectangulo</a></li>
+        <li><a href="/cliente/1/conversor_grados">Ejercicio 3: Conversor de grados</a></li>
+    </ul>
+
+    <h2> Ejercicios #3 </h2>
+    <ul>
+        <li><a href="/cliente/3/fecha"> Ejercicio 1: Fecha y hora actual </a></li>
+        <li><a href="/cliente/3/textoColorido"> Ejercicio 2: Cambiar el color de un texto </a></li>
+        <li><a href="/cliente/3/multiply"> Ejercicio 3: Multiplicar 2 números.</a></li>
+        <li><a href="/cliente/3/imagen"> Ejercicio 4: Cambiar una imagen con un botón. </a></li>
+    </ul>
+
+<?php
+include $VIEW_DIR . '/layouts/__footer.php';
+?>
